cd06e5eeefc4d549c6bb2246b53eea6d85b837ad
Nadhief Athallah Isya <nadhiefathallahi@gmail.com>
2025-05-05 12:49:32 +0700

    oke

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://192.168.1.65:8001/";
+export const API_BASE_URL = "http://localhost:8001/";
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
