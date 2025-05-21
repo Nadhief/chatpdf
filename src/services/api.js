108be7718f36389529ddd13de05826a4ea9b00ba
ShawFox <fadlidwiramadhan@gmail.com>
2025-05-21 11:49:07 +0700

    Merge from chatalizer

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = "192.168.1.65:8001/";
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-api.interceptors.request.use(
+api.interceptors.request.use(s
     (config) => {
       if (!(config.data instanceof FormData)) {
         config.headers["Content-Type"] = "application/json";
