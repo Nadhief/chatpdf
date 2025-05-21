c6cdb3a38e266f0881a09fcbcb66da746bff7b6e
ShawFox <fadlidwiramadhan@gmail.com>
2025-05-21 14:36:41 +0700

    fix endpoint personal

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const chatPersonal = async (payload) => {
   try {
-    const response = await api.post("chat/personal_v2/", payload);
+    const response = await api.post("chat/personal_v3/", payload);
     return response.data;
   } catch (error) {
     console.error("Error adding department:", error);
